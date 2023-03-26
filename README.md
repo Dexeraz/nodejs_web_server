bd644bdd92a87585dff0b8fdcf7fa1da5ad1f290
MBilik <dexeraz@gmail.com>
2023-03-26 14:19:43 +0200

    JWT and access token added

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 **Description:**
+Notes:
+> node
+> require('crypto').randomBytes(64).toString('hex')
 
 This repository shares the code applied during the Youtube tutorial. The tutorial is part of a [Node.js for Beginners Playlist](https://www.youtube.com/playlist?list=PL0Zuz27SZ-6PFkIxaJ6Xx_X46avTM1aYw)
--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const cors = require("cors");
 const corsOptions = require('./config/corsOptions')
 const { logger, logEvents } = require("./middleware/logEvents");
 const errorHandler = require("./middleware/errorHandler");
+const verifyJWT = require('./middleware/verifyJWT')
+const cookieParser = require('cookie-parser');
+const credentials = require("./middleware/credentials");
 const PORT = process.env.PORT || 3500;
 
 //custom middleware logger
@@ -13,6 +16,7 @@ app.use(logger);
 //Cross Origin Resource Sharing
 //Open to the public API
 //app.use(cors());
+app.use(credentials)
 
 //give acces the API for a specific domain
 
@@ -24,12 +28,19 @@ app.use(express.urlencoded({ extended: false }));
 //middleware for json data being submited
 app.use(express.json());
 
+//middleware for cookiers
+app.use(cookieParser());
+
 //serve static files
 app.use("/", express.static(path.join(__dirname, "/public")));
 
 //routes
 app.use("/", require("./routes/root"));
 app.use("/register", require("./routes/register"));
+app.use("/auth", require("./routes/auth"));
+app.use("/refresh", require("./routes/refresh"));
+//Everything after the line below will use verifyJWT to route (which is good)
+app.use(verifyJWT);
 app.use("/employees", require("./routes/api/employees"));
 
 //Different 404 depending on the file type request
